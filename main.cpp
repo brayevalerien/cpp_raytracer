47f54c2823f5104d6f2dbd73912d7a781286a432
Vanonym <60972074+valovanonym@users.noreply.github.com>
2022-12-29 01:48:45 -0500

    Created Scene and Sphere classes
    These two new classes are not used yet in the rest of the code

--- main.cpp
+++ main.cpp
@@ -4,24 +4,64 @@
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <vector>
+
 using namespace std;
 
-static HWND sHwnd;
-static COLORREF redColor=RGB(255,0,0);
-static COLORREF blueColor=RGB(0,0,255);
-static COLORREF greenColor=RGB(0,255,0);
-static int xRes = 1200; // image resolution
-static int yRes = 720; 
 
-void SetWindowHandle(HWND hwnd)
-{
+static HWND sHwnd;
+static COLORREF defaultColor = RGB(255, 0, 0); // will be used the default color for a pixel if not specified
+static int xRes = 1200; // image resolution in pixels
+static int yRes = 720;
+static float projPlaneHeight = 2880;
+static float projPlaneWidth = 4800;
+static float projPlaneDistance = 1; // distance between the projection plane and the camera
+
+
+class Sphere{
+    public:
+        tuple<float, float, float> center;
+        float radius;
+        COLORREF color;
+    
+        Sphere(tuple<float, float, float> center, float radius, COLORREF color) {
+            this->center = center;
+            this->radius = radius;
+            this->color = color;
+        }    
+};
+
+class Scene { // a scene that contains objects and a projection plane (viewport)
+    public:
+        // Projection plane properties
+        float projPlaneWidth;
+        float projPlaneHeight;
+        float projPlaneDistance;
+        vector<Sphere> spheres; // contains all spheres in the scene
+
+        Scene(float projPlaneWidth, float projPlaneHeight, float projPlaneDistance, vector<Sphere> spheres) {
+            this->projPlaneWidth = projPlaneWidth;
+            this->projPlaneHeight = projPlaneHeight;
+            this->projPlaneDistance = projPlaneDistance;
+            this->spheres = spheres;
+        }
+
+        Scene getDefaultScene() { // returns a default scene with three spheres
+            return Scene(1, 1, 1,
+                         {
+                            Sphere(make_tuple(0, -1, 3), 1.0, RGB(255, 0, 0)),
+                            Sphere(make_tuple(2, 0, 4), 1, RGB(0, 0, 255)),
+                            Sphere(make_tuple(-2, 0, 4), 1, RGB(0, 255, 0))
+                         });
+        }
+};
+
+void SetWindowHandle(HWND hwnd) {
     sHwnd=hwnd;
 }
 
-void setPixel(int x,int y, const COLORREF& color=redColor)
-{
-    if(sHwnd==NULL)
-    {
+void setPixel(int x,int y, const COLORREF& color=defaultColor) {
+    if(sHwnd==NULL) {
         MessageBox(NULL, L"sHwnd was not initialized !", L"Error",MB_OK|MB_ICONERROR);
         exit(0);
     }
@@ -31,7 +71,7 @@ void setPixel(int x,int y, const COLORREF& color=redColor)
     return;
 }
 
-std::tuple<float, float, float> canvasToViewport(int canvasX, int canvasY) {
+tuple<float, float, float> canvasToViewport(int canvasX, int canvasY) {
     /**
      * Convert a pixel position in the canvas into a 3D viewport position
      * 
@@ -39,9 +79,16 @@ std::tuple<float, float, float> canvasToViewport(int canvasX, int canvasY) {
      * @param canvasY The y coordinate in the canvas
      * @return a 3-upple of coordinates in the viewport
     */
-    float vpX, vpY, vpZ;
-    // TODO write the code that converts a pixel position in the canvas to a viewport position
-    return std::make_tuple(vpX, vpY, vpZ);
+    float vpX = canvasX * projPlaneWidth/xRes;
+    float vpY = canvasY * projPlaneHeight/yRes;
+    float vpZ = projPlaneDistance;
+    return make_tuple(vpX, vpY, vpZ);
+}
+
+tuple<int, int, int> viewportColor(float vpX, float vpY, float vpZ) {
+    float r, g, b;
+    // TODO write the code that computes the difuse color at the viewport position 
+    return make_tuple(r, g, b);
 }
 
 const COLORREF pixelColor(int x, int y) {
@@ -54,14 +101,8 @@ const COLORREF pixelColor(int x, int y) {
     return RGB(x%256, y%256, 0);
 }
 
-std::tuple<int, int, int> viewportColor(float vpX, float vpY, float vpZ) {
-    float r, g, b;
-    // TODO write the code that computes the difuse color at the viewport position 
-    return std::make_tuple(r, g, b);
-}
 
-void render()
-{
+void render() {
     for(int x = 0; x < xRes; x++) {
         for(int y = 0; y < yRes; y++) {
             COLORREF color = pixelColor(x, y);
@@ -70,14 +111,12 @@ void render()
     }
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
-{
-    switch(message)
-    {
+LRESULT CALLBACK WndProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam) {
+    switch(message) {
     case WM_PAINT:
         SetWindowHandle(hwnd);
         render();
-        std::cout << "Rendering complete." << std::endl;
+        cout << "Rendering complete." << endl;
         break;
     case WM_CLOSE: // Failure to call DefWindowProc
         break;
@@ -90,8 +129,7 @@ LRESULT CALLBACK WndProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
     return DefWindowProc(hwnd,message,wParam,lParam);
 }
 
-int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int iCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int iCmdShow) {
     static TCHAR szAppName[] = TEXT("3D Renderer");
     WNDCLASS wndclass;
     wndclass.style         = CS_HREDRAW | CS_VREDRAW;
@@ -106,8 +144,7 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,i
     wndclass.lpszClassName = szAppName;
     
     // Register the window
-    if(!RegisterClass(& wndclass))
-    {
+    if(!RegisterClass(& wndclass)) {
         MessageBox(NULL, L"Registering the class failed", L"Error",MB_OK|MB_ICONERROR);
         exit(0);
     }
@@ -123,20 +160,18 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,i
                         NULL,
                         hInstance,
                         NULL);
-    if(!hwnd)
-    {
+    if(!hwnd) {
         MessageBox(NULL, L"Window Creation Failed!", L"Error",MB_OK);
         exit(0);
     }
     
     // ShowWindow and UpdateWindow
-    ShowWindow(hwnd,iCmdShow);
+    ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
     
     // Message Loop
     MSG msg;
-    while(GetMessage(& msg,NULL,0,0))
-    {
+    while(GetMessage(& msg, NULL, 0, 0)) {
         TranslateMessage(& msg);
         DispatchMessage(& msg);
     }
